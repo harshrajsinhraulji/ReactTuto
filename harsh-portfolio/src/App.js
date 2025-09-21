50b48b1e6e710c5027f10f19e2a3896be2ed5476
harshrajsinhraulji <h.raulji2005@gmail.com>
2025-09-21 23:58:29 +0530

    feat: add hero to app

--- harsh-portfolio/src/App.js
+++ harsh-portfolio/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Navbar from './components/Navbar.js'; // Import the Navbar component
+import Navbar from './components/Navbar.js';
+import Hero from './components/Hero.js'; // 1. Import the Hero component
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> {/* Use the Navbar component here */}
+      <Navbar />
+      <Hero /> {/* 2. Add the Hero component here */}
       {/* The rest of your page sections will go below this */}
     </div>
   );
