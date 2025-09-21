1577c4275ca16b0a704fd63d06808185c0da09d2
harshrajsinhraulji <h.raulji2005@gmail.com>
2025-09-21 23:59:16 +0530

    update hero.js

--- harsh-portfolio/src/components/Hero.js
+++ harsh-portfolio/src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Hero.css';
-// import profilePic from '../assets/your-photo.jpg'; // We will add your photo in the next step
+// 1. Make sure this path and filename are correct
+import profilePic from '../assets/profile-photo.jpg'; 
 
 const Hero = () => {
   return (
@@ -13,8 +14,8 @@ const Hero = () => {
         <a href="#projects" className="hero-button">View My Work</a>
       </div>
       <div className="hero-image-container">
-        {/* <img src={profilePic} alt="Harshrajsinh Raulji" className="hero-image"/> */}
-        <div className="hero-image-placeholder"></div> {/* Placeholder for now */}
+        {/* 2. Replace the placeholder div with this img tag */}
+        <img src={profilePic} alt="Harshrajsinh Raulji" className="hero-image"/>
       </div>
     </section>
   );
